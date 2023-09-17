4b7944ebc6a33bd327f16ef0bb2786ef3367880a
周守欢 <shouhuan.zhou@xjsd.com>
2023-09-17 15:40:29 +0800

    rename id in layout_stub.xml

--- app/src/main/java/com/example/viewstubdemo/StubActivity.kt
+++ app/src/main/java/com/example/viewstubdemo/StubActivity.kt
@@ -9,7 +9,7 @@ import com.example.viewstubdemo.databinding.LayoutStubBinding
 
 class StubActivity : AppCompatActivity() {
     private lateinit var binding: ActivityStubBinding
-    private lateinit var stub1Binding: LayoutStubBinding
+    private lateinit var stubBinding: LayoutStubBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,7 +23,7 @@ class StubActivity : AppCompatActivity() {
     private fun ActivityStubBinding.initView() {
         viewStub.apply {
             setOnInflateListener { _, view ->
-                stub1Binding = LayoutStubBinding.bind(view)
+                stubBinding = LayoutStubBinding.bind(view)
             }
             inflate() //调用其inflate()的时候，其布局属性android:layout=”@layout/布局”将会替换这个ViewStub标签，这个时候我们使用findViewById将获取这个ViewStub对象为空
         }
@@ -31,13 +31,13 @@ class StubActivity : AppCompatActivity() {
 
     private fun ActivityStubBinding.setOnButtonListener() {
         showViewBtn.setOnClickListener {
-            stub1Binding.stubLayout1.visibility = View.VISIBLE
-            stub1Binding.stubImg.setImageResource(R.mipmap.ic_launcher)
+            stubBinding.stubLayout.visibility = View.VISIBLE
+            stubBinding.stubImg.setImageResource(R.mipmap.ic_launcher)
         }
 
         updateViewBtn.setOnClickListener {
-            if (stub1Binding.stubLayout1.visibility == View.VISIBLE) {
-                stub1Binding.stubImg.setImageResource(R.drawable.ic_launcher_background)
+            if (stubBinding.stubLayout.visibility == View.VISIBLE) {
+                stubBinding.stubImg.setImageResource(R.drawable.ic_launcher_background)
                 Toast.makeText(this@StubActivity, "图片已更换", Toast.LENGTH_SHORT)
                     .show()
             } else {
@@ -47,7 +47,7 @@ class StubActivity : AppCompatActivity() {
         }
 
         hideViewBtn.setOnClickListener {
-            stub1Binding.stubLayout1.visibility = View.GONE
+            stubBinding.stubLayout.visibility = View.GONE
         }
     }
 }
